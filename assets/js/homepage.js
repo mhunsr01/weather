aa70d5a8e388691bf9042b3a8154721af1849f77
Matt Sr Hunter <mhunsr@gmail.com>
2022-12-04 15:02:37 -0500

    adding code to get started.  getting organized.  I dont like this, but I will organize it.  need to get my thoughts better organized.

---
+++ assets/js/homepage.js
@@ -0,0 +1,102 @@
+var city = document.querySelector('#city');
+var searchedCityBtnsEl = document.querySelector('#searched');
+var cityInputEl = document.querySelector('#city');
+
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var forecast = city.value.trim();
+
+  if (username) {
+    getCityWeather(username);
+
+    city.textContent = '';
+    cityInputEl.value = '';
+  } else {
+    alert('Please enter a city');
+  }
+};
+
+var buttonClickHandler = function (event) {
+  var weather = event.target.getAttribute('weather');
+
+  if (weather) {
+    getFeaturedForecast(weather);
+
+    weather.textContent = '';
+  }
+};
+
+var getweather = function (user) {
+  var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          displayRepos(data, user);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to GitHub');
+    });
+};
+
+var getFeaturedRepos = function (language) {
+  var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: ' + response.statusText);
+    }
+  });
+};
+
+var displayRepos = function (repos, searchTerm) {
+  if (repos.length === 0) {
+    repoContainerEl.textContent = 'No repositories found.';
+    return;
+  }
+
+  repoSearchTerm.textContent = searchTerm;
+
+  for (var i = 0; i < repos.length; i++) {
+    var repoName = repos[i].owner.login + '/' + repos[i].name;
+
+    var repoEl = document.createElement('a');
+    repoEl.classList = 'list-item flex-row justify-space-between align-center';
+    repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
+
+    var titleEl = document.createElement('span');
+    titleEl.textContent = repoName;
+
+    repoEl.appendChild(titleEl);
+
+    var statusEl = document.createElement('span');
+    statusEl.classList = 'flex-row align-center';
+
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+    } else {
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    repoEl.appendChild(statusEl);
+
+    repoContainerEl.appendChild(repoEl);
+  }
+};
+
+userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener('click', buttonClickHandler);
